8fb22a96a589e7a05c712c4f19a2e29e3589d5bc
Typelias <98ille@gmail.com>
2019-09-02 15:36:35 +0200

    Added go server hosting. TODO: Fix server displaying blank page

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,8 @@ module github.com/Typelias/ChatTut
 
 go 1.12
 
-require github.com/gorilla/websocket v1.4.0
+require (
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/websocket v1.4.0
+)
--- backend/main.go
+++ backend/main.go
@@ -5,13 +5,70 @@ import (
 	"net/http"
 
 	"github.com/Typelias/ChatTut/pkg/websocket"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/securecookie"
 )
+
+var cookieHandler = securecookie.New(
+	securecookie.GenerateRandomKey(64),
+	securecookie.GenerateRandomKey(32))
+
+func getUserName(r *http.Request) (userName string) {
+	if cookie, err := r.Cookie("session"); err == nil {
+		cookieValue := make(map[string]string)
+		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+			userName = cookieValue["name"]
+		}
+	}
+	return userName
+}
+
+func setSession(userName string, w http.ResponseWriter) {
+	value := map[string]string{
+		"name": userName,
+	}
+	if encoded, err := cookieHandler.Encode("session", value); err == nil {
+		cookie := &http.Cookie{
+			Name:  "session",
+			Value: encoded,
+			Path:  "/",
+		}
+		http.SetCookie(w, cookie)
+	}
+}
+
+func clearSession(w http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
+	}
+	http.SetCookie(w, cookie)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	pass := r.FormValue("password")
+	redirectTarget := "/"
+	if name != "" && pass != "" {
+		setSession(name, w)
+		redirectTarget = "/internal"
+	}
+
+	http.Redirect(w, r, redirectTarget, 302)
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	clearSession(w)
+	http.Redirect(w, r, "/", 302)
+}
 
 func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Websocket Endpoint Hit")
 	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
-		fmt.Fprint(w, "%++v\n",err)
+		fmt.Fprint(w, "%++v\n", err)
 	}
 
 	client := &websocket.Client{
@@ -23,16 +80,60 @@ func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
 	client.Read()
 }
 
-func setUpRoutes() {
+const indexPage = `
+<h1>Login</h1>
+<form method="post" action="/login">
+    <label for="name">User name</label>
+    <input type="text" id="name" name="name">
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password">
+    <button type="submit">Login</button>
+</form>
+`
+
+func indexPageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, indexPage)
+}
+
+/*func setUpRoutes() {
 	pool := websocket.NewPool()
 	go pool.Start()
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request){
-		serveWs(pool,w,r)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
 	})
+}*/
+
+func internalPageHandler(w http.ResponseWriter, r *http.Request) {
+	userName := getUserName(r)
+	if userName != "" {
+		http.ServeFile(w, r, "../frontend/build/index.html")
+	} else {
+		http.Redirect(w, r, "/", 302)
+	}
+
 }
+
+var router = mux.NewRouter()
 
 func main() {
 	fmt.Println("Chatapp v:0.1")
-	setUpRoutes()
+
+	fs := http.FileServer(http.Dir("../frontend/build/staic"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	pool := websocket.NewPool()
+	go pool.Start()
+	router.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+
+	router.HandleFunc("/", indexPageHandler)
+	router.HandleFunc("/internal", internalPageHandler)
+
+	router.HandleFunc("/login", loginHandler).Methods("POST")
+	router.HandleFunc("/logout", logoutHandler).Methods("POST")
+	//setUpRoutes()
+
+	http.Handle("/", router)
 	http.ListenAndServe(":8080", nil)
 }
